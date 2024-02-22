33e890bb3d47bac7c2b46726b6964beae334a27a
WesleySchorel <112857487+WesleySchorel@users.noreply.github.com>
2024-02-22 13:27:37 +0100

    RSS Feed
    Changelog:
    - Banner test door middel van RSS Feed

---
+++ src/lib/assets/components/widgets/actualiteitbanner.svelte
@@ -0,0 +1,7 @@
+<script>
+	import { page } from '$app/stores';
+
+	const widgetPath = 'actualiteitbanner';
+</script>
+
+<iframe src="{$page.url.origin}/widgets/{widgetPath}" title="actualiteitbanner widget" frameborder="0"></iframe>
--- src/lib/index.js
+++ src/lib/index.js
@@ -1,1 +1,5 @@
 // place files you want to import through the `$lib` alias in this folder.
+export { default as Weer1 } from '$lib/assets/components/widgets/weer-1.svelte'
+export { default as Analogeklok1 } from '$lib/assets/components/widgets/analogeklok-1.svelte'
+export { default as Actualiteitbanner } from '$lib/assets/components/widgets/actualiteitbanner.svelte'
+
