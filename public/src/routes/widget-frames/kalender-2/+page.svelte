d4ae79b401cbdbbd9a1cbf950204f926699f7827
WesleySchorel <112857487+WesleySchorel@users.noreply.github.com>
2024-04-12 17:48:20 +0200

    Semantic changes
    Changelog:
    - Changed divs to sections

--- public/src/routes/widget-frames/kalender-2/+page.svelte
+++ public/src/routes/widget-frames/kalender-2/+page.svelte
@@ -5,21 +5,21 @@
 	let currentMonth = getDate.toLocaleDateString('nl-NL', { month: 'long' });
 </script>
 
-<div class="top">
+<section class="calendar-top">
 	<span>{currentMonth}</span>
-</div>
+</section>
 <div class="line"></div>
-<div class="layered-paper">
+<section class="layered-paper">
 	<h1>{currentDate}</h1>
 	<p>{currentWeekDay}</p>
-</div>
+</section>
 
 <style>
-	.top {
+	.calendar-top {
 		color: white;
 		text-transform: uppercase;
 		background-color: #009f66;
-        background-image: linear-gradient(to right, #009f66 , #008E5B);
+		background-image: linear-gradient(to right, #009f66, #008e5b);
 		border-radius: var(--border-radius-widget) var(--border-radius-widget) 0 0;
 
 		width: 100%;
@@ -32,10 +32,10 @@
 	}
 
 	.line {
-        border-top: 2px dashed #00764b;
-        background: #009f66;
-        background-image: linear-gradient(to right, #009f66 , #008E5B);
-        padding: 1px;
+		border-top: 2px dashed #00764b;
+		background: #009f66;
+		background-image: linear-gradient(to right, #009f66, #008e5b);
+		padding: 1px;
 	}
 
 	.layered-paper {
@@ -43,23 +43,23 @@
 		border-top: 0.25rem solid #00764b;
 		width: 100%;
 		box-shadow:
-			0 1px 1px rgba(0, 0, 0, 0.20),
+			0 1px 1px rgba(0, 0, 0, 0.2),
 			0 7px 0 0 white,
-			0 8px 1px 0 rgba(0, 0, 0, 0.20),
+			0 8px 1px 0 rgba(0, 0, 0, 0.2),
 			0 15px 0 0 white,
-			0 16px 1px 0 rgba(0, 0, 0, 0.20);
+			0 16px 1px 0 rgba(0, 0, 0, 0.2);
 		border-radius: 0 0 var(--border-radius-widget) var(--border-radius-widget);
 		display: flex;
 		flex-direction: column;
 		align-items: center;
 	}
 
-	h1 {
+	.layered-paper h1 {
 		font-size: 5rem;
 		margin-bottom: 2.7rem;
 	}
 
-	p {
+	.layered-paper p {
 		position: absolute;
 		font-size: 1.5rem;
 		margin: 2rem;
