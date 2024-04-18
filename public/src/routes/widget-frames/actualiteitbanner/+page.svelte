d1618e0338c4859e8be2c4855d66a66ca84a0fcb
tom-2810 <tom.poortman@hva.nl>
2024-04-18 16:17:35 +0200

    rebuild
    Changelog:
    
    - xml => xml substring Array
    - NOS
    - styling
    
    Co-Authored-By: Wesley <112857487+WesleySchorel@users.noreply.github.com>

--- public/src/routes/widget-frames/actualiteitbanner/+page.svelte
+++ public/src/routes/widget-frames/actualiteitbanner/+page.svelte
@@ -1,35 +1,23 @@
 <script>
+	import { pusher } from '$lib/index.js';
 	import { onMount } from 'svelte';
 
 	export let data;
-	const xmlString = data.xmlString;
 
-	let items = [];
-	onMount(async () => {
-		const parser = new DOMParser();
-		const xmlDOM = parser.parseFromString(xmlString, 'text/xml');
-
-		const rssItems = xmlDOM.querySelectorAll('item');
-
-		items = Array.from(rssItems)
-			.map((item) => ({
-				title: item.querySelector('title').textContent
-				// link: item.querySelector('link').textContent,
-				// description: item.querySelector('description').textContent,
-				// pubDate: item.querySelector('pubDate').textContent
-			}))
-			.filter((item) => !isRTLNieuws(item.title));
-
+	$: headlines = data.data;
+	onMount(() => {
 		setInterval(
 			() => (document.getElementById('currentTime').innerText = new Date().toLocaleTimeString()),
 			990
 		);
-	});
 
-	function isRTLNieuws(title) {
-		const rtlKeywords = ['rtl weer', 'rtl z', 'rtl nieuws', 'aflevering'];
-		return rtlKeywords.some((keyword) => title.toLowerCase().includes(keyword));
-	}
+		const actualiteitChannel = pusher.subscribe('actualiteit-channel');
+
+		actualiteitChannel.bind('update-actualiteit', (data) => {
+			console.log(data);
+			headlines = data.data;
+		});
+	});
 </script>
 
 <svelte:head>
@@ -41,15 +29,15 @@
 	<h1 id="currentTime"></h1>
 	<div class="banner">
 		<div class="banner-content">
-			{#each items as item}
-				<h2>{item.title}</h2>
-				-
+			{#each headlines as item}
+				<h2>{item}</h2>
+				<span>●</span>
 			{/each}
 		</div>
 		<div class="banner-content">
-			{#each items as item}
-				<h2>{item.title}</h2>
-				-
+			{#each headlines as item}
+				<h2>{item}</h2>
+				<span>●</span>
 			{/each}
 		</div>
 	</div>
@@ -63,28 +51,35 @@
 		scroll-behavior: smooth;
 	}
 
+	span {
+		display: flex;
+		align-items: center;
+		font-size: 0.8rem;
+		margin: 0 1rem 0 1rem;
+	}
+
 	#rss-feed {
+		position: relative;
+		/* top: 2px; */
+		/* height: fill-available; */
 		display: flex;
 		overflow: hidden;
+		background-color: #524040;
 	}
 
 	h1 {
 		color: white;
-		background-color: #404052;
 		text-transform: uppercase;
-		margin-right: 0;
-		z-index: 1;
-		padding: 0.3rem 0.6rem 0.3rem 0.6rem;
 		font-size: 2rem;
-		height: 3rem;
+		margin: auto 0.5rem auto 0.5rem;
+		z-index: 1;
 	}
 
 	h2 {
 		display: flex;
 		justify-content: center;
-		align-items: baseline;
+		align-items: center;
 		font-size: 2rem;
-		padding: 0.2rem;
 	}
 
 	.banner {
@@ -96,14 +91,13 @@
 		overflow: hidden;
 		z-index: 1;
 		font-size: 1.85rem;
-		height: 3rem;
+		height: calc(3rem + 2px);
 	}
 
 	.banner-content {
 		position: relative;
 		display: flex;
 		flex-direction: row;
-		gap: 2rem;
 		flex-shrink: 0;
 		animation: scroll 100s linear infinite;
 	}
@@ -113,7 +107,7 @@
 			transform: translateX(0);
 		}
 		to {
-			transform: translateX(calc(-100% - 0.5rem));
+			transform: translateX(calc(-100%));
 		}
 	}
 </style>
