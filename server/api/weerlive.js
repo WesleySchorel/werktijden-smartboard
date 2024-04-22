11b50a3f31947441703c8667d99165a111f7f1ca
tom-2810 <tom.poortman@hva.nl>
2024-04-22 21:59:31 +0200

    Update weerlive.js

--- server/api/weerlive.js
+++ server/api/weerlive.js
@@ -1,13 +1,16 @@
 module.exports = async function updateLiveWeer(pusher) {
-  console.log("--weather poll--");
-  const res = await fetch(
-    `https://weerlive.nl/api/weerlive_api_v2.php?key=demo&locatie=Amsterdam`
-  );
-  const data = await res.json();
+  async function poll(pusher) {
+    console.log("--weather poll--");
+    const res = await fetch(
+      `https://weerlive.nl/api/weerlive_api_v2.php?key=demo&locatie=Amsterdam`
+    );
+    const data = await res.json();
 
-  await pusher.trigger("liveweer-channel", "update-liveweer", {
-    data: data,
-  });
+    await pusher.trigger("liveweer-channel", "update-liveweer", {
+      data: data,
+    });
 
-  setTimeout(() => { updateLiveWeer(pusher) }, 180000)
+    setTimeout(function () { poll(pusher) }, 5000)
+  }
+  poll(pusher)
 };
