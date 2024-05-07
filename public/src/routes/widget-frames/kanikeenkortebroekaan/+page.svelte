844fb520e79ff549a588b7a7d2d4ed8ad3060703
WesleySchorel <112857487+WesleySchorel@users.noreply.github.com>
2024-05-07 16:01:13 +0200

    Kortebroek
    Changelog:
    - Kanikeenkortebroekaan widget

---
+++ public/src/routes/widget-frames/kanikeenkortebroekaan/+page.svelte
@@ -0,0 +1,49 @@
+<script>
+    import kortebroek from '$lib/assets/kortebroek.svg';
+    import langebroek from '$lib/assets/langebroek.svg';
+
+	export let data;
+
+	$: weatherData = data;
+
+	$: gtemp = Math.round(weatherData.liveweer[0].gtemp);
+
+	$: korteBroekAan = gtemp > 20;
+</script>
+
+<div class="widget">
+{#if korteBroekAan}
+<img src={kortebroek} alt="" />
+    <h1>JA</h1>
+	<p>Je kan een korte broek aan!</p>
+{:else}
+<img src={langebroek} alt="" />
+    <h1>NEE</h1>
+	<p>Je kan geen korte broek aan!</p>
+{/if}
+</div>
+
+<style>
+	.widget {
+		position: relative;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: space-between;
+		color: white;
+        background-color: black;
+		padding: 0.8rem;
+	}
+    h1 {
+        font-size: 10rem;
+        line-height: 8rem;
+    }
+    p {
+        font-size: 1.2rem;
+    }
+	img {
+        position: absolute;
+        height: 9rem;
+        width: auto;
+	}
+</style>
